31b4fc0b14fef73903dbfd1272d491b462f86e28
Marco <dead_legend@hotmail.com>
2020-06-29 14:35:11 +0200

    Delete previous play round function. Create new page with questions which will be opened when the player selects all categories and easy. For every question, there will be four options rather than an input field.

--- quiz.js
+++ quiz.js
@@ -8,118 +8,33 @@ let questions = ['What is the largest carnivore on land?', 'What is the second h
         
         'Which famous museum would you visit if you wanted to see the equally famous painting Mona Lisa?', 'What year did Martin Luther publish his 95 theses?'];
 
-    let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
         
-    function playRound() {
+const nature = document.getElementById('nature-and-science');
 
-        let answer = prompt(randomQuestion);
+const geography = document.getElementById('geography');
 
-        switch (true) {
+const sports = document.getElementById('sports');
 
-            case randomQuestion == 'What is the largest carnivore on land?' && answer.toLowerCase() == 'Polar Bear'.toLowerCase(): alert('Correct!');
+const culture = document.getElementById('culture');
 
-            break;
+const history = document.getElementById('history');
 
-            case randomQuestion == 'What is the largest carnivore on land?' && answer.toLowerCase() == !'Polar Bear'.toLowerCase(): alert('Wrong!');
+const allCategories = document.getElementById('all-categories');
 
-            break;
+const easy = document.getElementById('easy');
 
-            case randomQuestion == 'What is the second highest mountain in the world?' && answer.toLowerCase() == 'K2'.toLowerCase(): alert('Correct!');
+const moderate = document.getElementById('moderate');
 
-            break;
+const hard = document.getElementById('hard');
 
-            case randomQuestion == 'What is the second highest mountain in the world?' && answer.toLowerCase() == !'K2'.toLowerCase(): alert('Wrong!');
+const button = document.getElementById('play');
 
-            break;
+button.addEventListener('click', openNewTab);
 
-            case randomQuestion == 'How long is a period in ice hockey?' && answer.toLowerCase() == '20 Minutes'.toLowerCase() || answer.toLowerCase() == 'Twenty Minutes'.toLowerCase(): 
-            
-                alert('Correct!');
+function openNewTab() {
 
-            break;
+    if ( allCategories.checked == true && easy.checked == true ) {
 
-            case randomQuestion == 'How long is a period in ice hockey?' && answer.toLowerCase() == !'20 Minutes'.toLowerCase() && answer.toLowerCase() == !'Twenty Minutes'.toLowerCase(): 
-            
-                alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'Which famous French existentialist writer was awarded the Nobel Prize in Literature in 1964 and refused it?' && answer.toLowerCase() == 
-            
-                'Jean-Paul Sartre'.toLowerCase() || answer.toLowerCase() == 'Jean Paul Sartre'.toLowerCase() || answer.toLowerCase() == 'Sartre'.toLowerCase(): alert('Correct!');
-
-            break;
-
-            case randomQuestion == 'Which famous French existentialist writer was awarded the Nobel Prize in Literature in 1964 and refused it?' && answer.toLowerCase() == 
-            
-                !'Jean-Paul Sartre'.toLowerCase() && answer.toLowerCase() == !'Jean Paul Sartre'.toLowerCase() && answer.toLowerCase() == !'Sartre'.toLowerCase(): alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'Which war was fought mainly in central Europe between 1618 and 1648?' && answer.toLowerCase() == '30 Years War'.toLowerCase() || answer.toLowerCase() == 
-            
-                '30 Years\' War'.toLowerCase() || answer.toLowerCase() == 'Thirty Years War'.toLowerCase() || answer.toLowerCase() == 'Thirty Years\' War'.toLowerCase(): alert('Correct!');
-
-            break;
-
-            case randomQuestion == 'Which war was fought mainly in central Europe between 1618 and 1648?' && answer.toLowerCase() == !'30 Years War'.toLowerCase() && answer.toLowerCase() == 
-            
-                !'30 Years\' War'.toLowerCase() && answer.toLowerCase() == !'Thirty Years War'.toLowerCase() && answer.toLowerCase() == !'Thirty Years\' War'.toLowerCase(): alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'What is the name of Charles Darwin\'s famous book published in 1859?' && answer.toLowerCase() == 'On The Origin Of Species'.toLowerCase() || 
-            
-                answer.toLowerCase() == 'The Origin Of Species'.toLowerCase() || answer.toLowerCase() == 'Origin Of Species'.toLowerCase(): alert('Correct!');
-
-            break;
-
-            case randomQuestion == 'What is the name of Charles Darwin\'s famous book published in 1859?' && answer.toLowerCase() == !'On The Origin Of Species'.toLowerCase() && 
-            
-                answer.toLowerCase() == !'The Origin Of Species'.toLowerCase() && answer.toLowerCase() == !'Origin Of Species'.toLowerCase(): alert('Wrong!');
-
-            break;
- 
-            case randomQuestion == 'What Indonesian island shares its name with one of the biggest programming languages?' && answer.toLowerCase() == 'Java'.toLowerCase(): alert('Correct!');
-                
-            break;
-                
-            case randomQuestion == 'What Indonesian island shares its name with one of the biggest programming languages?' && answer.toLowerCase() == !'Java'.toLowerCase(): alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'What color is the card shown by a referee during a football/soccer game when sending a player off?' && answer.toLowerCase() == 'Red'.toLowerCase(): 
-            
-                alert('Correct!');
-                
-            break;
-                
-            case randomQuestion == 'What color is the card shown by a referee during a football/soccer game when sending a player off?' && answer.toLowerCase() == !'Red'.toLowerCase(): 
-            
-                alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'Which famous museum would you visit if you wanted to see the equally famous painting Mona Lisa?' && answer.toLowerCase() == 'The Louvre'.toLowerCase() || 
-            
-                answer.toLowerCase() == 'Louvre'.toLowerCase(): alert('Correct!');
-                
-            break;
-                
-            case randomQuestion == 'Which famous museum would you visit if you wanted to see the equally famous painting Mona Lisa?' && answer.toLowerCase() == !'The Louvre'.toLowerCase() && 
-            
-                answer.toLowerCase() == !'Louvre'.toLowerCase(): alert('Wrong!');
-
-            break;
-
-            case randomQuestion == 'What year did Martin Luther publish his 95 theses?' && answer.toLowerCase() == '1517': alert('Correct!'); 
-                
-            break;
-                
-            case randomQuestion == 'What year did Martin Luther publish his 95 theses?' && answer.toLowerCase() == !'1517': alert('Wrong!');
-
-            break;
-            
-        }
-
+        open("quiz-easy-all-categories.html");
     }
+}
